f92feb4e0bd177e3f8434ae2b51f87e423805cab
Paolo Pepito <npaolopepito@gmail.com>
2024-09-20 13:39:30 +0800

    Add firenvim plugin

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'glacambre/firenvim',
+    build = ":call firenvim#install(0)",
+    config = function()
+      vim.g.firenvim_config = {
+        localSettings = {
+          ['.*'] = { takeover = 'never' }
+        }
+      }
+    end,
+  },
+}
