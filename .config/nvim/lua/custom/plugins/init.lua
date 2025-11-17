5febe1ad3dd1b4ad6fa46ce8f8789e7a993bed71
Paolo Pepito <npaolopepito@gmail.com>
2025-11-17 12:24:26 +0800

    Configure Peek plugin

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -19,7 +19,11 @@ return {
       event = { "VeryLazy" },
       build = "deno task --quiet build:fast",
       config = function()
-          require("peek").setup()
+          require("peek").setup({
+              close_on_bdelete = false,
+              theme = 'light',
+              app = 'browser',
+          })
           vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
           vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
       end,
