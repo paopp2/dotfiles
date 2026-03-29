b33823fbea4919c1e68095eb3f498cee92731470
Paolo Pepito <npaolopepito@gmail.com>
2026-03-29 14:00:05 +0800

    Add tmux window management keybindings
    - prefix+M: break pane to new window
    - prefix+</>: shift window left/right
    - prefix+O: interactive window reorder via nvim popup

---
+++ .tmux/scripts/reorder-windows.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+tmp=$(mktemp)
+tmux list-windows -F '#{window_index}: #{window_name}' > "$tmp"
+
+nvim "$tmp"
+
+# Move all windows to high temp indices to avoid collisions
+offset=1000
+while IFS=: read -r idx _; do
+  tmux move-window -s :"$idx" -t :"$offset"
+  offset=$((offset + 1))
+done < "$tmp"
+
+# Move them back to final positions
+offset=1000
+pos=1
+while IFS=: read -r _ _; do
+  tmux move-window -s :"$offset" -t :"$pos"
+  offset=$((offset + 1))
+  pos=$((pos + 1))
+done < "$tmp"
+
+rm "$tmp"
